6c8c4cee7e72dacdf92e78ad9ed18222c7bbb304
4fr0c0d3 <ngigemoffat@gmail.com>
2019-09-02 20:59:04 +0300

    chore(setup): setup server
    - add graphql-yoga for server
    - add simple type def
    - add simple resolver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('Cool');
+import { GraphQLServer } from 'graphql-yoga';
+
+// TypeDefs
+const typeDefs = `
+    type Query {
+        hello: String!
+
+    }
+
+`
+
+// Resolvers
+const resolvers = {
+    Query: {
+        hello: () => '4fr0c0d3'
+    }
+}
+
+// Server
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+});
+
+server.start(() => {
+    console.log(`👽 Server running 👽`);
+});
